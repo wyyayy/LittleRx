3f13440bb27c85f20ce77154fbf0f1801b927bf2
jianjun.lin <jianjun.lin@baidao.com>
2016-10-20 14:32:19 +0800

    x

--- src/littlerx/Subscriber.java
+++ src/littlerx/Subscriber.java
@@ -1,16 +1,10 @@
 package littlerx;
 
-import littlerx.producer.Producer;
-
 /**
  * Created by iceGeneral on 2016/10/15.
  */
 public abstract class Subscriber<T> {
 
     public abstract void onNext(T t);
-
-    public void setProducer(Producer p) {
-
-    }
 
 }
